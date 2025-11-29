64a29c70c1de96d2c65b1cbef5042f075ef56834
vicky <pratamavr@gmail.com>
2025-11-29 22:58:51 +0800

    perbaikan controller

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -2,7 +2,7 @@
 
 namespace Modules\UserManagement\Http\Controllers;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Permission;
 use Modules\UserManagement\Constants\Permissions;
