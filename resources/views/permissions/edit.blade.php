09f7bee2acd3eb8a3db41cb0abe028c14be1940d
vicky <pratamavr@gmail.com>
2025-12-22 01:43:45 +0800

    updates

--- resources/views/permissions/edit.blade.php
+++ resources/views/permissions/edit.blade.php
@@ -9,9 +9,7 @@
   <div class="card-header text-end">
     <div class="float-start me-auto">
       <a href="{{ route('usermanagement.permissions.index') }}" class="btn btn-secondary">
-        <svg class="icon">
-          <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-arrow-thick-left') }}"></use>
-        </svg>
+        <i class="fas fa-fw fa-arrow-left"></i>
       </a>
     </div>
     <h5 class="card-title">{{ $permission->description}}</h5><span class="small ms-2">{{$permission->name}}</span>
@@ -38,9 +36,7 @@
       </div>
       <div class="pt-2 mt-4 border-top border-primary">
         <button type="submit" class="btn btn-block bg-success">
-          <svg class="icon me-2">
-            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-paper-plane') }}"></use>
-          </svg>
+          <i class="fas fa-fw fa-paper-plane"></i>
           Save
         </button>
       </div>
