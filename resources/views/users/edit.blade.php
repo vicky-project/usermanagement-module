2b0b8728d17e1cde367e6c89ba95b640dd50d59f
vicky <pratamavr@gmail.com>
2025-12-22 09:20:52 +0800

    updates

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -10,9 +10,7 @@
   <div class="card-header text-end">
     <div class="float-start me-auto">
       <a href="{{ route('usermanagement.users.index') }}" class="btn btn-secondary">
-        <svg class="icon">
-          <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-arrow-thick-left') }}"></use>
-        </svg>
+        <i class="fas fa-fw fa-arrow-left"></i>
       </a>
     </div>
     <h5 class="card-title">{{$user->name}}</h5><span class="small ms-2">{{$user->email}}</span>
@@ -78,9 +76,7 @@
       </div>
       <div class="pt-2 mt-4 border-top border-primary">
         <button class="btn btn-block bg-success" @disabled((new PermissionRegistry())->userCanNot(auth()->user(), Permissions::EDIT_USERS))>
-          <svg class="icon me-2">
-            <use xlink:href="{{ asset('vendors/@coreui/icons/svg/free.svg#cil-paper-plane') }}"></use>
-          </svg>
+          <i class="fas fa-fw fa-paper-plane"></i>
           Save
         </button>
       </div>
